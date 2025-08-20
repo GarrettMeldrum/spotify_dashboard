b900fc42887bf135de42e189a19e52416ce768f6
Garrett Meldrum <garrettmeldrum@icloud.com>
2025-08-20 09:31:28 -0600

    updating the read database file

--- read database.py
+++ read database.py
@@ -5,7 +5,10 @@ import sys
 sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
 
 
-conn = sqlite3.connect('c:/github/spotify_dashboard/spotify_history.db')
+
+db_path = Path(__file__).resolve().parent / "spotify_history.db"
+conn = sqlite3.connect(f"file:{db_path}?mode=ro", uri=True)
+conn.connect()
 cursor = conn.cursor()
 
 cursor.execute("SELECT * FROM spotify_history ORDER BY played_at ASC")
