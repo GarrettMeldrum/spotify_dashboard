828c3482501d5c3ea8ceed971499598b2e5b298f
GarrettMeldrum <garrettmeldrum@icloud.com>
2026-04-05 17:21:26 -0600

    typo

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from datetime import datetime
 load_dotenv()
 CLIENT_ID = os.getenv('SPOTIFY_CLIENT_ID')
 CLIENT_SECRET = os.getenv('SPOTIFY_CLIENT_SECRET')
-REDIRECT_URI = os.getenv('SPOTFIY_REDIRECT_URI')
+REDIRECT_URI = os.getenv('SPOTIFY_REDIRECT_URI')
 DB = os.getenv('DB')
 
 # If the database/tables don't exists, create it based on the schema.sql
